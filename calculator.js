45f6e837f066b6f6c930962dba3e8269748a8249
ayewhosthat <alan.notiverson@gmail.com>
2024-05-24 08:04:42 -0400

    Add basic operations + function to extract parts of an expression, as well as the operate() function

--- calculator.js
+++ calculator.js
@@ -1,4 +1,32 @@
-const add = (a,b) => {a+b};
-const subtract = (a,b) => {a-b};
-const mulitply = (a,b) => {a*b};
-const divide = (a,b) => {a/b};
+const add = (a,b) => {return a+b};
+const subtract = (a,b) => {return a-b};
+const mulitply = (a,b) => {return a*b};
+const divide = (a,b) => {return a/b};
+
+const extractOperationParts = function(operation) {
+    operation = operation.replace(' ', '').split('');
+    // create new object with keys num1, operator, num2
+    const operationInfo = {
+        'num1': operation[0],
+        'operator': operation[1],
+        'num2': operation[2],
+    };
+    return operationInfo;
+};
+
+const operate = (operator, num1, num2) => {
+    switch (operator) {
+        case '+': return add(num1, num2);
+        case '-': return subtract(num1, num2);
+        case '*': return mulitply(num1, num2);
+        case '/': return divide(num1, num2);
+    }
+}
+
+// create event listener for number keys that will populate the display
+const addNumberBehaviour = function() {
+    const display = document.querySelector('.display');
+    const currNumber = display.textContent;
+    const numbers = document.querySelectorAll('.number')
+    console.log(numbers);
+}
