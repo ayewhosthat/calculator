af369f20223017c7d13ee1a5bbe0792f2d9b9188
ayewhosthat <alan.notiverson@gmail.com>
2024-05-25 19:16:42 -0400

    Add code handling case where user divides by 0

--- calculator.js
+++ calculator.js
@@ -9,7 +9,14 @@ const OPS = ['*', '/', '+', '-', '='];
 const add = (a,b) => {return a+b};
 const subtract = (a,b) => {return a-b};
 const multiply = (a,b) => {return a*b};
-const divide = (a,b) => {return a/b};
+const divide = (a,b) => {
+    if (b === 0) {
+        return 'XD';
+        resetDisplay();
+    } else {
+        return a/b;
+    }
+};
 
 const truncateDisplay = function() {
     if (display.textContent.length > 18) {
